7e7a183c4562d06c71ba0c2a89e90c31507d9dbf
ryanz <ryanzhou63@gmail.com>
2023-09-28 22:06:55 -0400

    updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/github_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/github_test.java
@@ -5,9 +5,12 @@ public class github_test {
         System.out.println("github test");
     }
 
-
+    //putting files into github:
     //Step 1: install git
     //Step 2: make changes
     //Step 3: commit changes
     //Step 4: push changes
+
+    //getting updated files from github:
+    //update project, (ctrl t)
 }
