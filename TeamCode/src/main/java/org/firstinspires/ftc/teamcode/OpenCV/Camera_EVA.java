44182b4d69d3b00b7db1c4a51152750ca0787a7b
Themiika <126718397+Themiika@users.noreply.github.com>
2023-10-03 10:22:40 -0400

    Build1.25

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/Camera_EVA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/Camera_EVA.java
@@ -123,7 +123,7 @@ public class Camera_EVA extends LinearOpMode
                             }
                         }catch (EnumConstantNotPresentException e){
                             telemetry.addLine("Error " + e);
-                            ///
+                            //
                         }
                     }
                 }
