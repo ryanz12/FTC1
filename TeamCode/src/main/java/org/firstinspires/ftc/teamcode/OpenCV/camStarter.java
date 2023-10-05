bb23ff5cb5c846d6e56094b6b1dc2c3003b00b69
Themiika <126718397+Themiika@users.noreply.github.com>
2023-10-05 16:25:15 -0400

    Build 1.0

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/camStarter.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.OpenCV;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class camStarter extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+//        initialize camera and pipeline
+        colorOpenCV cv = new colorOpenCV(this);
+//      call the function to startStreaming
+        cv.observeStick();
+        waitForStart();
+        while (opModeIsActive()) {
+        }
+//        stopStreaming
+        cv.stopCamera();
+    }
+
+}
