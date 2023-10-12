9f4a5acc17ce999a4614f7efe706c8a91b7b87aa
Themiika <126718397+Themiika@users.noreply.github.com>
2023-10-11 20:03:27 -0400

    Build 2.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/colorOpenCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/colorOpenCV.java
@@ -37,26 +37,27 @@ public class colorOpenCV extends LinearOpMode{
             @Override
             public void onOpened()
             {
+                /*
+                 * Tell the webcam to start streaming images to us! Note that you must make sure
+                 * the resolution you specify is supported by the camera. If it is not, an exception
+                 * will be thrown.
+                 *
+                 * Keep in mind that the SDK's UVC driver (what OpenCvWebcam uses under the hood) only
+                 * supports streaming from the webcam in the uncompressed YUV image format. This means
+                 * that the maximum resolution you can stream at and still get up to 30FPS is 480p (640x480).
+                 * Streaming at e.g. 720p will limit you to up to 10FPS and so on and so forth.
+                 *
+                 * Also, we specify the rotation that the webcam is used in. This is so that the image
+                 * from the camera sensor can be rotated such that it is always displayed with the image upright.
+                 * For a front facing camera, rotation is defined assuming the user is looking at the screen.
+                 * For a rear facing camera or a webcam, rotation is defined assuming the camera is facing
+                 * away from the user.
+                 */
                 webcam.setPipeline(opencv);
                 //start streaming the camera
                 webcam.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
                 //if you are using dashboard, update dashboard camera view
                 /*FtcDashboard.getInstance().startCameraStream(webcam, 5);*/
-                try{
-                    if(true){
-                        // Autonomous code
-                        telemetry.addLine("HAS FOUND THING RUN AUTONOMOUS CODE");
-                    }
-                    else{
-                        // Look for object
-                        telemetry.addLine("HAS NOT FOUND THING RUN AUTONOMOUS CODE");
-
-                    }
-                }catch (Exception e){
-                    telemetry.addLine("CHECK THE WONKY BOOLEAN CODE");
-                    telemetry.addLine(e.getMessage());
-                }
-
             }
 
             @Override
