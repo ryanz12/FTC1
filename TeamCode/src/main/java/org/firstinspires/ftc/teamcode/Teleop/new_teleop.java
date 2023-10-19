a0a2a2086609eb693469d068aec8797628c44bae
ryanz <ryanzhou63@gmail.com>
2023-10-19 15:59:32 -0400

    Build ftc lib 0.00003

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/new_teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/new_teleop.java
@@ -9,22 +9,46 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
 public class new_teleop extends LinearOpMode {
+
+    boolean isMoving;
+
+    MecanumDrive drive = new MecanumDrive(
+            new Motor(hardwareMap, "LeftFrontMotor", Motor.GoBILDA.RPM_312),
+            new Motor(hardwareMap, "RightFrontMotor", Motor.GoBILDA.RPM_312),
+            new Motor(hardwareMap, "LeftBackMotor", Motor.GoBILDA.RPM_312),
+            new Motor(hardwareMap, "RightBackMotor", Motor.GoBILDA.RPM_312)
+    );
+
+    //move robot when dpad forward
+    GamepadEx gamepad = new GamepadEx(gamepad1);
+
     @Override
-    public void runOpMode() throws InterruptedException{
-        MecanumDrive drive = new MecanumDrive(
-                new Motor(hardwareMap, "LeftFrontMotor", Motor.GoBILDA.RPM_435),
-                new Motor(hardwareMap, "RightFrontMotor", Motor.GoBILDA.RPM_435),
-                new Motor(hardwareMap, "LeftBackMotor", Motor.GoBILDA.RPM_435),
-                new Motor(hardwareMap, "RightBackMotor", Motor.GoBILDA.RPM_435)
-        );
+    public void runOpMode(){
 
-        //move robot when dpad forward
-        GamepadEx gamepad = new GamepadEx(gamepad1);
+        while(opModeIsActive()){
+            if(gamepad.getButton(GamepadKeys.Button.DPAD_UP)){
+                drive.driveRobotCentric(0, 1, 0);
+                isMoving = true;
+            }
+            else if(gamepad.getButton(GamepadKeys.Button.DPAD_DOWN)){
+                drive.driveRobotCentric(0, -1, 0);
+                isMoving = true;
+            }
+            else if(gamepad.getButton(GamepadKeys.Button.DPAD_LEFT)){
+                drive.driveRobotCentric(1, 0, 0);
+                isMoving = true;
+            }
+            else if(gamepad.getButton(GamepadKeys.Button.DPAD_RIGHT)){
+                drive.driveRobotCentric(-1, 0, 0);
+                isMoving = true;
+            }
+            else{
+                drive.driveRobotCentric(0, 0, 0);
+                isMoving = false;
+            }
 
-        if(gamepad.getButton(GamepadKeys.Button.DPAD_UP)){
-            drive.driveRobotCentric(0, 1, 0);
-        }else{
-            drive.driveRobotCentric(0, 0, 0);
+            telemetry.addData("Robot", isMoving);
+            telemetry.update();
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/teleop.java
@@ -10,8 +10,8 @@ public class teleop extends LinearOpMode {
     private DcMotor rightFrontMotor;
     private DcMotor leftBackMotor;
     private DcMotor rightBackMotor;
-    private DcMotor armMotorLeft;
-    private DcMotor armMotorRight;
+//    private DcMotor armMotorLeft;
+//    private DcMotor armMotorRight;
 
 
     public double tgtPowerlfw;
@@ -26,8 +26,8 @@ public class teleop extends LinearOpMode {
         rightFrontMotor = hardwareMap.get(DcMotor.class, "RightFrontMotor");
         leftBackMotor = hardwareMap.get(DcMotor.class, "LeftBackMotor");
         rightBackMotor = hardwareMap.get(DcMotor.class, "RightBackMotor");
-        armMotorLeft = hardwareMap.get(DcMotor.class, "armMotorLeft");
-        armMotorRight = hardwareMap.get(DcMotor.class, "armMotorRight");
+//        armMotorLeft = hardwareMap.get(DcMotor.class, "armMotorLeft");
+//        armMotorRight = hardwareMap.get(DcMotor.class, "armMotorRight");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -49,10 +49,13 @@ public class teleop extends LinearOpMode {
             else if(gamepad2.dpad_right){
                 moveMotors("strafeRight");
             }
-            else if(gamepad2.left_stick_x < 0){
-                moveMotors("turnLeft");
+            else if(gamepad2.dpad_left){
+                moveMotors("strafeRight");
             }
             else if(gamepad2.left_stick_x > 0){
+                moveMotors("turnLeft");
+            }
+            else if(gamepad2.left_stick_x < 0){
                 moveMotors("turnRight");
             }
             else if(gamepad2.left_bumper){
@@ -70,8 +73,8 @@ public class teleop extends LinearOpMode {
             rightFrontMotor.setPower(tgtPowerrfw);
             leftBackMotor.setPower(tgtPowerlbw);
             rightBackMotor.setPower(tgtPowerrbw);
-            armMotorLeft.setPower(armTgtPower);
-            armMotorRight.setPower(armTgtPower);
+//            armMotorLeft.setPower(armTgtPower);
+//            armMotorRight.setPower(armTgtPower);
 
             telemetry.addData("Status", "Running");
             telemetry.addData("Status", "compliing?");
@@ -82,36 +85,42 @@ public class teleop extends LinearOpMode {
 
     public void moveMotors(String str){
         if(str == "forward"){
+            telemetry.addData("Direction", "Forward");
             tgtPowerlfw = 1;
-            tgtPowerrfw = 1;
-            tgtPowerlbw = 1;
+            tgtPowerrfw = -1;
+            tgtPowerlbw = -1;
             tgtPowerrbw = 1;
         }
         else if(str == "backward"){
+            telemetry.addData("Direction", "Backward");
             tgtPowerlfw = -1;
-            tgtPowerrfw = -1;
-            tgtPowerlbw = -1;
-            tgtPowerrbw = -1;
-        }
-        else if(str == "strafeRight"){
-            tgtPowerlfw = 1;
-            tgtPowerrfw = -1;
+            tgtPowerrfw = 1;
             tgtPowerlbw = 1;
             tgtPowerrbw = -1;
         }
-        else if(str == "strafeLeft"){
+        else if(str == "strafeRight"){
+            telemetry.addData("Direction", "StrafeRight");
             tgtPowerlfw = -1;
             tgtPowerrfw = 1;
             tgtPowerlbw = -1;
             tgtPowerrbw = 1;
         }
+        else if(str == "strafeLeft"){
+            telemetry.addData("Direction", "StrafeLeft");
+            tgtPowerlfw = 1;
+            tgtPowerrfw = -1;
+            tgtPowerlbw = 1;
+            tgtPowerrbw = -1;
+        }
         else if(str == "turnLeft"){
+            telemetry.addData("Direction", "Turn Left");
             tgtPowerlfw = 1;
             tgtPowerlbw = 1;
             tgtPowerrfw = -1;
             tgtPowerrbw = -1;
         }
         else if(str == "turnRight"){
+            telemetry.addData("Direction", "Turn Right");
             tgtPowerlfw = -1;
             tgtPowerlbw = -1;
             tgtPowerrfw = 1;
@@ -124,7 +133,6 @@ public class teleop extends LinearOpMode {
             armTgtPower = 1;
         }
     }
-
 
     public void resetMotors(){
         tgtPowerlfw = 0;
