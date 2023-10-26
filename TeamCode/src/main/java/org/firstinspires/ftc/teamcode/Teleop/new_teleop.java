a24c7d3e76e39ea08ed69fb7e80d99a084deb740
Themiika <drampwee@gmail.com>
2023-10-26 19:46:05 -0400

    Build 5.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/new_teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/new_teleop.java
@@ -19,51 +19,51 @@ public class new_teleop extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        armLeft = hardwareMap.get(DcMotor.class, "leftFront");
-        armRight = hardwareMap.get(DcMotor.class, "rightFront");
+//        armLeft = hardwareMap.get(DcMotor.class, "leftFront");
+//        armRight = hardwareMap.get(DcMotor.class, "rightFront");
+//
+//        armLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        armRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        armLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-//        Motor leftBack = new Motor(hardwareMap, "leftBack", Motor.GoBILDA.RPM_312);
-//        leftBack.setInverted(true);
-//        MecanumDrive drive = new MecanumDrive(
-//                new Motor(hardwareMap, "leftFront", Motor.GoBILDA.RPM_312),
-//                new Motor(hardwareMap, "rightFront", Motor.GoBILDA.RPM_312),
-//                leftBack,
-//                new Motor(hardwareMap, "rightBack", Motor.GoBILDA.RPM_312)
-//        );
+        Motor leftBack = new Motor(hardwareMap, "leftBack", Motor.GoBILDA.RPM_312);
+        leftBack.setInverted(true);
+        MecanumDrive drive = new MecanumDrive(
+                new Motor(hardwareMap, "leftFront", Motor.GoBILDA.RPM_312),
+                new Motor(hardwareMap, "rightFront", Motor.GoBILDA.RPM_312),
+                leftBack,
+                new Motor(hardwareMap, "rightBack", Motor.GoBILDA.RPM_312)
+        );
 
         GamepadEx driverOp = new GamepadEx(gamepad1);
         GamepadEx driverArm = new GamepadEx(gamepad2);
 
         waitForStart();
 
-        while (!isStopRequested()) {
-//            drive.driveRobotCentric(
-//                    -driverOp.getLeftX(),
-//                    driverOp.getLeftY(),
-//                    -driverOp.getRightX()
-//            );
+//        while (!isStopRequested()) {
+////            drive.driveRobotCentric(
+////                    -driverOp.getLeftX(),
+////                    driverOp.getLeftY(),
+////                    -driverOp.getRightX()
+////            );
 
-            if(gamepad1.a){
-                telemetry.addData("Left Bumper Pressed", "True");
-
-                armLeft.setTargetPosition(1400);
-                armRight.setTargetPosition(1400);
-
-                armLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                armRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-                while(armLeft.isBusy() && armRight.isBusy()){
-                    telemetry.addData("Motors are mpoving", "true");
-                }
-
-                armLeft.setPower(0);
-                armRight.setPower(0);
-
-                telemetry.update();
-            }
+//            if(gamepad1.a){
+//                telemetry.addData("Left Bumper Pressed", "True");
+//
+//                armLeft.setTargetPosition(1400);
+//                armRight.setTargetPosition(1400);
+//
+//                armLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//                armRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//
+//                while(armLeft.isBusy() && armRight.isBusy()){
+//                    telemetry.addData("Motors are mpoving", "true");
+//                }
+//
+//                armLeft.setPower(0);
+//                armRight.setPower(0);
+//
+//                telemetry.update();
+//            }
         }
     }
 
