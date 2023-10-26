7acd899a156aa8930e2949c1fbee029c228e9297
Themiika <drampwee@gmail.com>
2023-10-26 19:53:06 -0400

    Build 5.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/new_teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/new_teleop.java
@@ -11,17 +11,8 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class new_teleop extends LinearOpMode {
-
-
-    static final boolean FIELD_CENTRIC = false;
-
     @Override
     public void runOpMode() throws InterruptedException {
-//        armLeft = hardwareMap.get(DcMotor.class, "leftFront");
-//        armRight = hardwareMap.get(DcMotor.class, "rightFront");
-//
-//        armLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        armRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         Motor leftBack = new Motor(hardwareMap, "leftBack", Motor.GoBILDA.RPM_312);
         leftBack.setInverted(true);
@@ -44,25 +35,6 @@ public class new_teleop extends LinearOpMode {
             );
 
         }
-//            if(gamepad1.a){
-//                telemetry.addData("Left Bumper Pressed", "True");
-//
-//                armLeft.setTargetPosition(1400);
-//                armRight.setTargetPosition(1400);
-//
-//                armLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                armRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//
-//                while(armLeft.isBusy() && armRight.isBusy()){
-//                    telemetry.addData("Motors are mpoving", "true");
-//                }
-//
-//                armLeft.setPower(0);
-//                armRight.setPower(0);
-//
-//                telemetry.update();
-        //
-//            }
-        }
     }
+}
 
