09188e9ea8fc4d47f6fd8a1a68d7317beb493c39
Themiika <drampwee@gmail.com>
2023-10-26 19:50:28 -0400

    Build 5.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/new_teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/new_teleop.java
@@ -12,8 +12,6 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp
 public class new_teleop extends LinearOpMode {
 
-    public DcMotor armLeft;
-    public DcMotor armRight;
 
     static final boolean FIELD_CENTRIC = false;
 
@@ -24,7 +22,7 @@ public class new_teleop extends LinearOpMode {
 //
 //        armLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 //        armRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//
+
         Motor leftBack = new Motor(hardwareMap, "leftBack", Motor.GoBILDA.RPM_312);
         leftBack.setInverted(true);
         MecanumDrive drive = new MecanumDrive(
