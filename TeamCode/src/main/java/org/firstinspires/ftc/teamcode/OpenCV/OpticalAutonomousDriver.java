8565a44a98fb09cfe9ed55ea48691df0003b5e9b
Themiika <drampwee@gmail.com>
2023-11-01 16:21:12 -0400

    Build 6.5

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/OpticalAutonomousDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/OpticalAutonomousDriver.java
@@ -21,12 +21,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 //Themika pipeline and camera.
-//Sean and ryan Movemnt and roadrunner
+//Sean and ryan Movement and roadrunner
 @Autonomous
-public class OpticalAutonomousDriver extends OpMode {
-
+public class OpticalAutonomousDriver extends LinearOpMode {
+//Change LinearOpMode If necessary
     OpenCvWebcam webcam = null;
-
+    public enum loc{
+        Left,
+        Right,
+        NotFound
+    }
+    private  loc location;
     private DcMotor leftFrontMotor;
     private DcMotor rightFrontMotor;
     private DcMotor leftBackMotor;
@@ -38,16 +43,12 @@ public class OpticalAutonomousDriver extends OpMode {
     boolean canSeeLeft = false;
 
     @Override
-    public void init() {
+    public void runOpMode() throws InterruptedException {
         WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam");
         int cameraViewID = hardwareMap.appContext.getResources().getIdentifier("cameraViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName,cameraViewID);
-        webcam.setPipeline(new directionColorPipeline());
-
-        leftFrontMotor = hardwareMap.get(DcMotor.class, "leftFront");
-        rightFrontMotor = hardwareMap.get(DcMotor.class, "rightFront");
-        leftBackMotor = hardwareMap.get(DcMotor.class, "leftBack");
-        rightBackMotor = hardwareMap.get(DcMotor.class, "rightBack");
+        YellowDetector detector = new YellowDetector(telemetry);
+        webcam.setPipeline(new ColorPipeline());
         webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
@@ -59,89 +60,19 @@ public class OpticalAutonomousDriver extends OpMode {
                 telemetry.addLine("HSHSHHHDHDHDHDHDHD");
             }
         });
-    }
 
-    @Override
-    public void loop() {
-
-    }
-
-    class directionColorPipeline extends OpenCvPipeline {
-
-        //Creates a YCbc mat which is YCbCr represents color as brightness and two color difference signals, while RGB represents color as red, green and blue. In YCbCr, the Y is the brightness
-        Mat YCbC = new Mat();
-        //Left side mat
-        Mat lCrop;
-        //Right side mat
-        Mat rCrop;
-        //Left average final variable which is used to identify that the left side has more of the color.
-        double leftAvgFin = 0;
-        //Rght average final variable which is used to identify that the left side has more of the color.
-        double rightAvgFin = 0;
-        //Boolean which identify which directional path to go.
-        public boolean canSeeLeft;
-        public boolean canSeeRight;
-        public boolean canTSee;
-
-        //Mat too see out put
-        Mat output = new Mat();
-        //Color can be changed to any color knowing rbg values right now it is red
-        Scalar rectColor = new Scalar(255.0,0.0,0);
-        @Override
-        public Mat processFrame(Mat input) {
-            //Converts the input mat to preferred color type
-            Imgproc.cvtColor(input,YCbC,Imgproc.COLOR_RGB2YCrCb);
-
-            //Splits the screen into two using a function similar to javas rect function
-            org.opencv.core.Rect leftRect = new org.opencv.core.Rect(1,1,319,479);
-            org.opencv.core.Rect rightRect = new Rect(320,1,319,479);
-
-            //To see the actual split on the camera stream
-            input.copyTo(output);
-            Imgproc.rectangle(output,leftRect,rectColor,2);
-            Imgproc.rectangle(output,rightRect,rectColor,2);
-
-            //Create a submat of all to detect the color
-            lCrop = YCbC.submat(leftRect);
-            rCrop = YCbC.submat(rightRect);
-            //Finds the average amount of color on both sides
-            Scalar leftAvg = Core.mean(lCrop);
-            telemetry.addData("LeftAvg", leftAvg);
-            Scalar rightAvg = Core.mean(rCrop);
-            telemetry.addData("RightAvg", rightAvg);
-
-            //The zero element is where the avg is stored.
-            leftAvgFin = leftAvg.val[0];
-            rightAvgFin = rightAvg.val[0];
-
-            //Idea
-            /*
-            SO basically first we turn the robot to see color. Once identified turn fully that
-             way. Place pixel. Turn to center the robot. Move the robot around the Object.
-             Then stop using that camera pipeline and switch to the april tag pipeline.
-             */
-            //Finds where the color is most prominent
-            if(leftAvgFin > rightAvgFin){
-                telemetry.addLine("Yay left");
-                canSeeLeft = true;
-                //To move left
-            }
-            else if(leftAvgFin < rightAvgFin){
-                telemetry.addLine("Yay right");
-                canSeeRight = true;
-                //to move right
-            }
-            else if(leftAvgFin < 130 || rightAvgFin <130){
-                telemetry.addLine("Can't see");
-                canSeeMiddle = true;
-            }
-            else{
-                telemetry.addLine("Middle");
-            }
-            //returns the split screen
-            return(output);
-
+        waitForStart();
+        switch (detector.getLocation()) {
+            case LEFT:
+                // ...
+                break;
+            case RIGHT:
+                // ...
+                break;
+            case NOT_FOUND:
+                // ...
         }
+        webcam.stopStreaming();
 
     }
 
