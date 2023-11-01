38cc90cf075a8980ceabdf39892c787572440ae6
Themiika <drampwee@gmail.com>
2023-11-01 17:53:35 -0400

    Build 7.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/new_teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/new_teleop.java
@@ -11,28 +11,19 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class new_teleop extends LinearOpMode {
-    private DcMotor armLeft = null;
-    private DcMotor armRight = null;
     @Override
     public void runOpMode() throws InterruptedException {
 
         Motor leftBack = new Motor(hardwareMap, "leftBack", Motor.GoBILDA.RPM_312);
         leftBack.setInverted(true);
+        Motor rightBack = new Motor(hardwareMap, "rightBack", Motor.GoBILDA.RPM_312);
+        rightBack.setInverted(true);
         MecanumDrive drive = new MecanumDrive(
                 new Motor(hardwareMap, "leftFront", Motor.GoBILDA.RPM_312),
                 new Motor(hardwareMap, "rightFront", Motor.GoBILDA.RPM_312),
                 leftBack,
-                new Motor(hardwareMap, "rightBack", Motor.GoBILDA.RPM_312)
+                rightBack
         );
-
-        armLeft = hardwareMap.get(DcMotor.class, "armLeft");
-        armRight = hardwareMap.get(DcMotor.class, "armRight");
-
-        armLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        armRight.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        armLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        armRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         GamepadEx driverOp = new GamepadEx(gamepad1);
 
@@ -41,40 +32,13 @@ public class new_teleop extends LinearOpMode {
         while (!isStopRequested()) {
             drive.driveRobotCentric(
                     -driverOp.getLeftX(),
-                     driverOp.getLeftY(),
+                    driverOp.getLeftY(),
                     -driverOp.getRightX()
             );
 
-            if(gamepad1.a){
-                moveArm();
-            }
         }
     }
 
-    public void moveArm(){
-        if(opModeIsActive()){
-            armLeft.setTargetPosition(2000);
-            armRight.setTargetPosition(2000);
-
-            armLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            armRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            armLeft.setPower(1);
-            armRight.setPower(1);
-
-            while(opModeIsActive() && (armLeft.isBusy() && armRight.isBusy())){
-                telemetry.addData("Running to",  " %7d :%7d", 2000,  2000);
-                telemetry.addData("Currently at",  " at %7d :%7d",
-                        armLeft.getCurrentPosition(), armRight.getCurrentPosition());
-                telemetry.update();
-            }
-
-            armLeft.setPower(0);
-            armRight.setPower(0);
-
-            armLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            armRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
-    }
 }
+
 
