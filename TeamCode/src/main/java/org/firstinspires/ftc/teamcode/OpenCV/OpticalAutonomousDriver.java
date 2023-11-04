dbdda9db24953c632721f2e7bb3f8742164b519f
Themiika <drampwee@gmail.com>
2023-11-04 17:21:44 -0400

    Build 10.4

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/OpticalAutonomousDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/OpticalAutonomousDriver.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.OpenCV;
 
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
