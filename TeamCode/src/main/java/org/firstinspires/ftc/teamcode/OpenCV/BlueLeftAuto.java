238f332e61e590437ef045b2f29ddaff7f433405
Themiika <126718397+Themiika@users.noreply.github.com>
2023-11-24 15:02:24 -0500

    Build 20.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/BlueLeftAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/BlueLeftAuto.java
@@ -90,7 +90,7 @@ public class BlueLeftAuto extends LinearOpMode {
         WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
         int cameraViewID = hardwareMap.appContext.getResources().getIdentifier("cameraViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName,cameraViewID);
-        YellowDetector detector = new YellowDetector(telemetry);
+        blueDetector detector = new blueDetector(telemetry);
         webcam.setPipeline(detector);
 
         //making the trajectory
