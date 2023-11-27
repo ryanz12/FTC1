7a970824f4871dda22abf45bfd9a2dedc4588729
ryanz <ryanzhou63@gmail.com>
2023-11-27 18:12:07 -0500

    9.8

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/armTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/armTest.java
@@ -35,16 +35,20 @@ public class armTest extends LinearOpMode {
         while(!isStopRequested()){
 
             if(gamepad1.right_bumper){
-                armLeft.setPower(1);
-            }else{
-                armLeft.setPower(0);
+                armMove = !armMove;
+                if(armMove){
+                    moveArm(800, 0.3);
+                }
+                else{
+                    moveArm(0,.15);
+                }
             }
         }
     }
 
 
     //arm mehtod
-    public void moveArm(int ticks){
+    public void moveArm(int ticks, double speed){
         if(opModeIsActive()){
             armLeft.setTargetPosition(ticks);
             armRight.setTargetPosition(ticks);
@@ -52,15 +56,14 @@ public class armTest extends LinearOpMode {
             armLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             armRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-            armLeft.setPower(.4);
-            armRight.setPower(.4);
+            armLeft.setPower(speed);
+            armRight.setPower(speed);
 
             while(opModeIsActive() && (armLeft.isBusy() && armRight.isBusy())){
                 telemetry.addData("Running to",  " %7d :%7d", ticks,  ticks);
                 telemetry.addData("Currently at",  " at %7d :%7d",
                         armLeft.getCurrentPosition(), armRight.getCurrentPosition());
                 telemetry.update();
-
 
             }
 
