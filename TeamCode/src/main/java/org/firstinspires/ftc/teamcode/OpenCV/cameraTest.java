82c2e617114ec6074a3e20b94364653ac123c54f
Themiika <126718397+Themiika@users.noreply.github.com>
2023-12-02 15:25:39 -0500

    Build 30.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/cameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/cameraTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.OpenCV;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -18,6 +19,7 @@ import org.openftc.easyopencv.OpenCvWebcam;
 //hey!!
 //Themika pipeline and camera.
 //Sean and ryan Movement and roadrunner
+@Disabled
 @Autonomous
 public class cameraTest extends LinearOpMode {
 //Change LinearOpMode If necessary
